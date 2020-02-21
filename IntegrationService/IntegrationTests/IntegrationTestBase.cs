ac463507517d93bf17342dfb95594827a156de4b
Brian Ahern <aherntb@gmail.com>
2020-02-21 18:22:36 +0000

    Initial checkin of all files

---
+++ IntegrationService/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationService.IntegrationTests
+{
+    public class IntegrationTestBase
+    {
+        
+    }
+}
---
+++ IntegrationService/Program.cs
@@ -0,0 +1,15 @@
+﻿using IntegrationService.IntegrationTests;
+using System;
+
+namespace IntegrationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegrationTestBase testBase = new IntegrationTestBase();
+            testBase.CreateDatabase();
+            testBase.RemoveDatabase();
+        }
+    }
+}
